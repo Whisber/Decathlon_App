dd966cbe9b1fa060a0c695d7d4366fd2743ec066
Whisber <Whisber1@gmail.com>
2022-04-26 02:18:02 +0300

    Proje dosyası ekle.

---
+++ Decathlon_App/Decathlon_App/AppShell.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms;
+
+namespace Decathlon_App
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
